151e2ac022bfa3e6ad296c02ec1f617ff65aa6b4
denispyr <pyr.school@gmail.com>
2021-05-11 19:20:40 +0300

    Add MetricItem: usee css

--- src/components/MetricItem.js
+++ src/components/MetricItem.js
@@ -1,10 +1,14 @@
+import "./MetricItem.css"
+
 const MetricItem = () => {
-    return <div>
-        <div>11/5 17:30</div>
+    return <div className="metric-item">
+    <div className="metric-item__date">11/5 17:30</div>
+    <div className="metric-item__metric">
         <div>Vassiliki D</div>
-        <div>StockMarket Desktop App</div>
-        <div>5</div>
+        <h3>StockMarket Mobile App</h3>
     </div>
+    <div className="metric-item__count">16</div>
+</div>
 }
 
 export default MetricItem
