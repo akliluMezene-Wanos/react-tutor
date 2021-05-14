3c91411bfe9b94e15f5b59992f9600826f4b94a1
denispyr <pyr.school@gmail.com>
2021-05-14 16:53:48 +0300

    MetricItem ~ use an array of objects

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import MetricItem from "./components/MetricItem"
 
 function App() {
-  let metricDate=new Date(2021, 4, 12, 17, 30, 0, 0);
-  let metricDeveloper = "Aklilu"
-  let project = "StockMarket Mobile App"
-  let count = 33
+  const metrics = [
+    {
+      date: new Date(2021, 4, 12, 17, 39, 0, 0),
+      developer: "Aklilu",
+      project: "StockMarket Backend App",
+      count: 55,
+    },
+    {
+      date: new Date(2021, 4, 11, 19, 32, 0, 0),
+      developer: "Joni",
+      project: "Fanzine Mobile App",
+      count: 44,
+    },
+    {
+      date: new Date(2021, 4, 11, 18, 40, 0, 0),
+      developer: "Theodoros",
+      project: "Fanzine Mobile App",
+      count: 33,
+    },
+    {
+      date: new Date(2021, 4, 12, 17, 30, 0, 0),
+      developer: "Joni",
+      project: "StockMarket Backend App",
+      count: 22,
+    },
+    {
+      date: new Date(2021, 4, 12, 15, 23, 0, 0),
+      developer: "Theodoros",
+      project: "StockMarket Mobile App",
+      count: 11,
+    }
+  ]
 
   return (
     <div>
       <h1>Hello React!</h1>
-      <MetricItem
-        count={count}
-        project={project}
-        developer={metricDeveloper}
-        date={metricDate}
-      />
+      <MetricItem count={metrics[0].count} project={metrics[0].project} developer={metrics[0].developer} date={metrics[0].date} />
+      <MetricItem count={metrics[1].count} project={metrics[1].project} developer={metrics[1].developer} date={metrics[1].date} />
+      <MetricItem count={metrics[2].count} project={metrics[2].project} developer={metrics[2].developer} date={metrics[2].date} />
+      <MetricItem count={metrics[3].count} project={metrics[3].project} developer={metrics[3].developer} date={metrics[3].date} />
+      <MetricItem count={metrics[4].count} project={metrics[4].project} developer={metrics[4].developer} date={metrics[4].date} />
     </div>
 
   );
